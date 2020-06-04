871d5fddfe7ece77011edabc92258cffa343f77f
Esteban <jose.gonzalez.h@est.una.ac.cr>
2020-06-04 17:15:15 -0600

    Se crea lo necesario para el proyecto de front end en web

---
+++ FrontendWeb/src/main/java/cr/una/frontendWeb/FrontEndApplication.java
@@ -0,0 +1,15 @@
+package cr.una.frontendWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Esteban
+ * @created 04/06/2020 - 05:13 p. m.
+ */
+@SpringBootApplication
+public class FrontEndApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FrontEndApplication.class, args);
+    }
+}
