74acf2c175a9e31e8dc4c446019e3fcec0f9eaba
Esteban <jose.gonzalez.h@est.una.ac.cr>
2020-06-05 15:01:02 -0600

    Se logra filtrar por cedula, número de cita y por nombre

--- FrontendWeb/src/main/java/cr/una/frontendWeb/controller/AppointmentController.java
+++ FrontendWeb/src/main/java/cr/una/frontendWeb/controller/AppointmentController.java
@@ -5,7 +5,9 @@ import cr.una.frontendWeb.sevice.Service;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 /**
@@ -17,10 +19,37 @@ public class AppointmentController {
     private Service service = new Service();
 
     @GetMapping("/appointments")
-    public String appointments(Model model)  {
+    public String appointments(Model model) {
         List<Patient> arrayAppointments = null;
         try {
             arrayAppointments = service.allAppointments();
+            model.addAttribute("arrayAppointments", arrayAppointments);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "appointments";
+    }
+
+    @PostMapping("/appointments")
+    public String appointments(Model model, HttpServletRequest httpServletRequest) {
+        String filterByName = httpServletRequest.getParameter("nameTxt");
+        String filterByNumber = httpServletRequest.getParameter("numberTxt");
+        String filterById = httpServletRequest.getParameter("idTxt");
+        List<Patient> arrayAppointments = null;
+        try {
+            if (filterById != "") {
+                arrayAppointments = service.searchById(filterById);
+            } else {
+                if (filterByName != "") {
+                    arrayAppointments = service.searchByName(filterByName);
+                } else {
+                    if (filterByNumber != "") {
+                        arrayAppointments = service.searchByNumber(Integer.parseInt(filterByNumber));
+                    } else {
+                        arrayAppointments = service.allAppointments();
+                    }
+                }
+            }
             model.addAttribute("arrayAppointments", arrayAppointments);
         } catch (Exception e) {
             e.printStackTrace();
