2cbe8d6b1f1176d6b2799978f235847c505dfbd2
Esteban <jose.gonzalez.h@est.una.ac.cr>
2020-06-05 11:39:47 -0600

    Se logra filtrar todas las citas

--- FrontendWeb/src/main/java/cr/una/frontendWeb/controller/AppointmentController.java
+++ FrontendWeb/src/main/java/cr/una/frontendWeb/controller/AppointmentController.java
@@ -2,6 +2,7 @@ package cr.una.frontendWeb.controller;
 
 import cr.una.frontendWeb.model.Patient;
 import cr.una.frontendWeb.sevice.Service;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -11,15 +12,16 @@ import java.util.List;
  * @author Esteban
  * @created 05/06/2020 - 09:18 a. m.
  */
+@Controller
 public class AppointmentController {
     private Service service = new Service();
 
     @GetMapping("/appointments")
     public String appointments(Model model)  {
-        List<Patient> appointments = null;
+        List<Patient> arrayAppointments = null;
         try {
-            appointments = service.allAppointments();
-            model.addAttribute("appointments", appointments);
+            arrayAppointments = service.allAppointments();
+            model.addAttribute("arrayAppointments", arrayAppointments);
         } catch (Exception e) {
             e.printStackTrace();
         }
---
+++ FrontendWeb/src/test/java/TestingDao.java
@@ -0,0 +1,23 @@
+import cr.una.frontendWeb.model.Patient;
+import cr.una.frontendWeb.sevice.Service;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Esteban
+ * @created 05/06/2020 - 10:26 a. m.
+ */
+public class TestingDao {
+    @Test
+    public void listarCitas() throws Exception {
+        Service service = new Service();
+        List<Patient> citas = service.allAppointments();
+        assertNotNull(citas);
+        for (Patient patient : citas) {
+            System.out.println(patient.toString());
+        }
+    }
+}
